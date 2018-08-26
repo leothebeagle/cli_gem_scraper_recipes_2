1dc24d98ee6c10d7b7cc7f08821cc119ee9dc7dd
Aziz Kosber <a.kosber@gmail.com>
2018-08-26 00:50:21 +0000

    creates a Recipe class with a class constructor

---
+++ lib/recipe.rb
@@ -0,0 +1,33 @@
+class Recipe
+  attr_accessor :name, :description, :url
+
+  @@top_recipes = []
+
+  def self.top_recipes
+    @@top_recipes
+  end
+
+  def save
+    self.class.top_recipes << self
+  end
+
+  def create_from_hash(recipe_hash) #you are given a recipe hash, which you will iterate over and
+    recipe_hash.each do |recipe_name, recipe_attributes|
+      recipe = Recipe.new
+      recipe.name = recipe_name
+      recipe.description = recipe_attributes[:description]
+      recipe.url = recipe_attributes[:url]
+      recipe.save
+    end
+
+  end
+end
+
+
+
+
+# recipes = {
+#   :pasta => {:description => "lovely pasta", :url => "https://www.pasta.com"},
+#   :rice => {:description => "fluffy, Persian rice", :url => "https://www.rice.com"},
+#   :meatballs => {:description => "Dawud Basha", :url => "https://www.dawudbasha.com"}
+# }
