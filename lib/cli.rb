a51520fa4bed7a755ef4f5eb0605d8326a20a764
Aziz Kosber <a.kosber@gmail.com>
2018-08-26 23:33:04 +0000

    defines the menu method for the cli controller

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,6 +3,7 @@ class CliController
   def call
     puts "Hi, there. Welcome to the recipe app!!The top recipes for this week are:"
     list_recipes
+    menu
   end
 
   def list_recipes
@@ -13,5 +14,19 @@ class CliController
     end
   end
 
+  def menu
+    input = nil
+    while input != "exit"
+      puts "If you'd like the ingredient list for a recipe, type in the recipe number. To view the recipe list, type list or type exit"
+      input = gets.strip.downcase
 
+      if input.to_i > 0 && input.to_i.between?(1, @recipes.length)
+        puts @recipes[input.to_i - 1]
+      elsif input == "list"
+        list_recipes
+      else
+        puts "not sure what you want, enter the recipe number, list, or exit"
+      end
+    end
+  end
 end
