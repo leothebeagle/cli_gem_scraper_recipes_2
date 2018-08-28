67dbf1ba8e7e757482e496b1cc69a36d38e0ca72
Aziz Kosber <a.kosber@gmail.com>
2018-08-28 18:20:59 +0000

    sets name convention. instructions = ing+dir

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,13 +16,13 @@ class Scraper #here is where you will pull the data and put it into a hash forma
   end
 
   def self.scrape_recipe_directions(recipe_url) #this is a method that'll extract instructions for a single recipe. this method will actually get called by another metho in Recipe.
-    recipe_directions = Nokogiri::HTML(open(recipe_url))
+    recipe_instructions = Nokogiri::HTML(open(recipe_url))
     instructions = { :ingredients => [], :directions => "" }
 
     # instructions[:steps] = recipe_directions.css("ul.steps li.step p").text #fetches the instructions and places them in the hash
-    instructions[:directions] = recipe_directions.css("li.step p").text #there are several formats to the recipes. some have gifs and ordered lists whereas others are input as unordered lists.
+    instructions[:directions] = recipe_instructions.css("li.step p").text #there are several formats to the recipes. some have gifs and ordered lists whereas others are input as unordered lists.
 
-    recipe_directions.css("ul.ingredients__group li.ingredient").each do |ingredient| #the initial css selector gets all the ingredient blocks. it then extracts the text and pushes that into the ingredients array in the instructions hash
+    recipe_instructions.css("ul.ingredients__group li.ingredient").each do |ingredient| #the initial css selector gets all the ingredient blocks. it then extracts the text and pushes that into the ingredients array in the instructions hash
       instructions[:ingredients] << ingredient.text
     end
     instructions
