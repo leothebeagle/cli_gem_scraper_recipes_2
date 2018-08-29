b630df31b8c264205f1fb71c3e23951da23ab64a
Aziz Kosber <a.kosber@gmail.com>
2018-08-29 02:27:20 +0000

    refactors Scraper #scrape_ba_recipes to instantiate recipes

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,17 +2,27 @@
 class Scraper #here is where you will pull the data and put it into a hash format. the hash will then be used by Recipe to instantiate new recipe objects
 
   def self.scrape_ba_recipes
+    # bonappetit_recipes = Nokogiri::HTML(open("https://www.bonappetit.com/recipes"))
+    # recipes = {}
+    #
+    # bonappetit_recipes.css("ul.cards li.cards__li").each do |recipe|
+    #   recipe_name = recipe.css("h1.card-hed").text
+    #   recipes[recipe_name.to_sym] = {
+    #     :description => recipe.css("div.card-body p.card-copy").text,
+    #     :url => "https://www.bonappetit.com/" + recipe.css("h1.card-hed a").attribute("href").value
+    #   }
+    # end
+    # recipes
     bonappetit_recipes = Nokogiri::HTML(open("https://www.bonappetit.com/recipes"))
-    recipes = {}
 
     bonappetit_recipes.css("ul.cards li.cards__li").each do |recipe|
-      recipe_name = recipe.css("h1.card-hed").text
-      recipes[recipe_name.to_sym] = {
-        :description => recipe.css("div.card-body p.card-copy").text,
-        :url => "https://www.bonappetit.com/" + recipe.css("h1.card-hed a").attribute("href").value
-      }
+      new_recipe = Recipe.new
+      new_recipe.name = recipe.css("h1.card-hed").text
+      new_recipe.description = recipe.css("div.card-body p.card-copy").text
+      new_recipe.url = "https://www.bonappetit.com/" + recipe.css("h1.card-hed a").attribute("href").value
+      new_recipe.save
     end
-    recipes
+
   end
 
   def self.scrape_recipe_instructions(recipe_url) #this is a method that'll extract instructions for a single recipe. this method will actually get called by another metho in Recipe.
