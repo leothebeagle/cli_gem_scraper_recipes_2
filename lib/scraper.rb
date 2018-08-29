996d4c31adca999c51816dc06535d7c91ecfcb4d
Aziz Kosber <a.kosber@gmail.com>
2018-08-29 02:39:13 +0000

    refactors Scraper #scrape_recipe_instructions to avoid hash

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,16 +25,29 @@ class Scraper #here is where you will pull the data and put it into a hash forma
 
   end
 
-  def self.scrape_recipe_instructions(recipe_url) #this is a method that'll extract instructions for a single recipe. this method will actually get called by another metho in Recipe.
+  def self.scrape_recipe_instructions #this is a method that'll extract instructions for a single recipe. this method will actually get called by another metho in Recipe.
+  Recipe.top_recipes.each do |recipe|
+    recipe_url = recipe.url
+    recipe.ingredients = []
+
     recipe_instructions = Nokogiri::HTML(open(recipe_url))
-    instructions = { :ingredients => [], :directions => "" }
+    recipe.directions = recipe_instructions.css("li.step p").text
 
-    # instructions[:steps] = recipe_directions.css("ul.steps li.step p").text #fetches the instructions and places them in the hash
-    instructions[:directions] = recipe_instructions.css("li.step p").text #there are several formats to the recipes. some have gifs and ordered lists whereas others are input as unordered lists.
-
-    recipe_instructions.css("ul.ingredients__group li.ingredient").each do |ingredient| #the initial css selector gets all the ingredient blocks. it then extracts the text and pushes that into the ingredients array in the instructions hash
-      instructions[:ingredients] << ingredient.text
+    recipe_instructions.css("ul.ingredients__group li.ingredient").each do |ingredient|
+      recipe.ingredients << ingredient.text
     end
-    instructions
   end
 end
+
+end
+#   recipe_instructions = Nokogiri::HTML(open(recipe_url))
+#   instructions = { :ingredients => [], :directions => "" }
+#
+#   # instructions[:steps] = recipe_directions.css("ul.steps li.step p").text #fetches the instructions and places them in the hash
+#   instructions[:directions] = recipe_instructions.css("li.step p").text #there are several formats to the recipes. some have gifs and ordered lists whereas others are input as unordered lists.
+#
+#   recipe_instructions.css("ul.ingredients__group li.ingredient").each do |ingredient| #the initial css selector gets all the ingredient blocks. it then extracts the text and pushes that into the ingredients array in the instructions hash
+#     instructions[:ingredients] << ingredient.text
+#   end
+#   instructions
+# end
