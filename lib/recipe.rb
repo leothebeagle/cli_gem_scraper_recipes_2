ec48cf7c3fcd1180c9581b4aa486def8268a7ed8
Aziz Kosber <a.kosber@gmail.com>
2018-08-29 04:18:39 +0000

    deletes all commented out code thats been refactored

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -10,37 +10,4 @@ class Recipe #the only other class that Recipes will interact with is Scraper. I
   def save
     self.class.top_recipes << self
   end
-
-  # def self.create_from_hash(recipe_hash) #you are given a recipe hash, which you will iterate over and instantiate new Recipe objects.
-  #   recipe_hash.each do |recipe_name, recipe_attributes|
-  #     recipe = Recipe.new
-  #     recipe.name = recipe_name.to_s
-  #     recipe.description = recipe_attributes[:description]
-  #     recipe.url = recipe_attributes[:url]
-  #     recipe.save
-  #   end
-  # end
-
-  # def self.create_from_ba_scrape
-  #   scraped_hash = Scraper.scrape_ba_recipes
-  #   self.create_from_hash(scraped_hash)
-  # end
-
-  # def self.add_recipe_instructions
-  #   self.top_recipes.each do |recipe|
-  #     recipe_url = recipe.url
-  #     instructions = Scraper.scrape_recipe_instructions(recipe_url)
-  #     recipe.ingredients = instructions[:ingredients]
-  #     recipe.directions = instructions[:directions]
-  #   end
-  # end
 end
-
-
-
-
-# recipes = {
-#   :pasta => {:description => "lovely pasta", :url => "https://www.pasta.com"},
-#   :rice => {:description => "fluffy, Persian rice", :url => "https://www.rice.com"},
-#   :meatballs => {:description => "Dawud Basha", :url => "https://www.dawudbasha.com"}
-# }
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,17 +2,6 @@
 class Scraper #here is where you will pull the data and put it into a hash format. the hash will then be used by Recipe to instantiate new recipe objects
 
   def self.scrape_ba_recipes
-    # bonappetit_recipes = Nokogiri::HTML(open("https://www.bonappetit.com/recipes"))
-    # recipes = {}
-    #
-    # bonappetit_recipes.css("ul.cards li.cards__li").each do |recipe|
-    #   recipe_name = recipe.css("h1.card-hed").text
-    #   recipes[recipe_name.to_sym] = {
-    #     :description => recipe.css("div.card-body p.card-copy").text,
-    #     :url => "https://www.bonappetit.com/" + recipe.css("h1.card-hed a").attribute("href").value
-    #   }
-    # end
-    # recipes
     bonappetit_recipes = Nokogiri::HTML(open("https://www.bonappetit.com/recipes"))
 
     bonappetit_recipes.css("ul.cards li.cards__li").each do |recipe|
@@ -25,29 +14,17 @@ class Scraper #here is where you will pull the data and put it into a hash forma
 
   end
 
-  def self.scrape_recipe_instructions #this is a method that'll extract instructions for a single recipe. this method will actually get called by another metho in Recipe.
-  Recipe.top_recipes.each do |recipe|
-    recipe_url = recipe.url
-    recipe.ingredients = []
+  def self.scrape_recipe_instructions #this is a method that'll extract instructions for a single recipe.
+    Recipe.top_recipes.each do |recipe|
+      recipe_url = recipe.url
+      recipe.ingredients = []
 
-    recipe_instructions = Nokogiri::HTML(open(recipe_url))
-    recipe.directions = recipe_instructions.css("li.step p").text
+      recipe_instructions = Nokogiri::HTML(open(recipe_url))
+      recipe.directions = recipe_instructions.css("li.step p").text
 
-    recipe_instructions.css("ul.ingredients__group li.ingredient").each do |ingredient|
-      recipe.ingredients << ingredient.text
+      recipe_instructions.css("ul.ingredients__group li.ingredient").each do |ingredient|
+        recipe.ingredients << ingredient.text
+      end
     end
   end
 end
-
-end
-#   recipe_instructions = Nokogiri::HTML(open(recipe_url))
-#   instructions = { :ingredients => [], :directions => "" }
-#
-#   # instructions[:steps] = recipe_directions.css("ul.steps li.step p").text #fetches the instructions and places them in the hash
-#   instructions[:directions] = recipe_instructions.css("li.step p").text #there are several formats to the recipes. some have gifs and ordered lists whereas others are input as unordered lists.
-#
-#   recipe_instructions.css("ul.ingredients__group li.ingredient").each do |ingredient| #the initial css selector gets all the ingredient blocks. it then extracts the text and pushes that into the ingredients array in the instructions hash
-#     instructions[:ingredients] << ingredient.text
-#   end
-#   instructions
-# end
